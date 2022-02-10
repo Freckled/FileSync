1ae0bd441da8f7a01cf66f228e2c3cc2531826d2
Freckled <mail@freckled.nl>
2022-02-10 19:56:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FileSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Server server = new Server();
+            Client client = new Client();
+            //server.start();
+            client.start();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
