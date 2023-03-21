7ea888f5b955f05dce5d71cde6007d30a1bc52bb
wesleyvB <wesleyvanburik@gmail.com>
2023-03-21 19:03:41 +0100

    add response codes

--- ResponseCode.cs
+++ ResponseCode.cs
@@ -9,16 +9,35 @@ namespace FileSync
 {
     internal class ResponseCode
     {
-        //public static readonly 125 = 125;
+        private readonly static Dictionary<int, string> responseCodes = new Dictionary<int, string>()
+        {
+            { 125, "Data connection already open; transfer starting. " },
+            { 550, "Requested action not taken. File unavailable (e.g., file not found, no access)." },
+            { 552, "Requested file action aborted. Exceeded storage allocation (for current directory or dataset)." },
+            { 553, "Requested action not taken. File name not allowed." },
+            { 502, "Command not implemented." },
+            { 250, "Requested file action okay, completed." },
+            { 225, "stop" }
+        };
 
         /// <summary>
         /// IsValid validates wether the process can continue.
         /// </summary>
         /// <param name="responseCode"></param>
         /// <returns>Boolean indicating true when the process can continue; false when to stop.</returns>
-        public static Boolean isValid(int responseCode = 0)
+        public static Boolean isValid(int _responseCode = 0)
         {
             return true;
+        }
+
+        public static int getResponseCode(string _responseStr)
+        {
+            return ResponseCode.responseCodes.FirstOrDefault(x => x.Value == _responseStr).Key;
+        }
+
+        public static string getResponseText(int _responseCode)
+        {
+            return ResponseCode.responseCodes[_responseCode];
         }
     }
 }
